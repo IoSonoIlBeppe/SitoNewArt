42790f56e39296b8beaf3999d9747e562350b3da
IlBeppe <matteo.faggian12@gmail.com>
2018-02-08 18:26:19 +0100

    Aggiunto products + hover dropdown navbar

--- index.php
+++ index.php
@@ -16,44 +16,42 @@
 </head>
 <body>
 	<!-- navbar -->
-	<div class="example3">
-		<nav class="navbar navbar-inverse navbar-static-top ">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="http://disputebills.com"><img src="https://res.cloudinary.com/candidbusiness/image/upload/v1455406304/dispute-bills-chicago.png" alt="Dispute Bills">
-					</a>
-				</div>
-				<div id="navbar3" class="navbar-collapse collapse">
-					<ul class="nav navbar-nav navbar-right">
-						<li class="active"><a href="index.php">Home</a></li>
-						<li><a href="about.php">Chi siamo</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Prodotti <span class="caret"></span></a>
-							<ul class="dropdown-menu" role="menu">
-								<li><a href="#">Tele</a></li>
-								<li><a href="#">Specchiere</a></li>
-								<li><a href="#">Orologi</a></li>
-								<!--<li class="divider"></li>
-								<li class="dropdown-header">Nav header</li>
-								<li><a href="#">Separated link</a></li>
-								<li><a href="#">One more separated link</a></li>-->
-							</ul>
-						</li>
-						<li><a href="#">Contatti</a></li>
-						
-					</ul>
-				</div>
-				<!--/.nav-collapse -->
+	<nav class="homenav navbar navbar-inverse navbar-static-top ">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="http://disputebills.com"><img src="https://res.cloudinary.com/candidbusiness/image/upload/v1455406304/dispute-bills-chicago.png" alt="Dispute Bills">
+				</a>
 			</div>
-			<!--/.container-fluid -->
-		</nav>
-	</div>
+			<div id="navbar3" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="about.php">Chi siamo</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Prodotti <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Tele</a></li>
+							<li><a href="#">Specchiere</a></li>
+							<li><a href="#">Orologi</a></li>
+							<!--<li class="divider"></li>
+							<li class="dropdown-header">Nav header</li>
+							<li><a href="#">Separated link</a></li>
+							<li><a href="#">One more separated link</a></li>-->
+						</ul>
+					</li>
+					<li><a href="#">Contatti</a></li>
+
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+		<!--/.container-fluid -->
+	</nav>
 	<!-- end navbar -->
 
 	<!-- carousel -->
