844820c947965fcaf905ef185b6862a0fd144d83
IlBeppe <matteo.faggian12@gmail.com>
2018-02-20 16:02:57 +0100

    Agiunta struttura pagina contatti

---
+++ about.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="">
+	<meta name="author" content="Matteo Faggian">
+
+	<title>New Art Production</title>
+
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+	<!-- navbar -->
+	<nav class="homenav navbar navbar-inverse navbar-static-top ">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar3">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="http://disputebills.com"><img src="https://res.cloudinary.com/candidbusiness/image/upload/v1455406304/dispute-bills-chicago.png" alt="Dispute Bills">
+				</a>
+			</div>
+			<div id="navbar3" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav navbar-right">
+					<li class="active"><a href="index.php">Home</a></li>
+					<li><a href="about.php">Chi siamo</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Prodotti <span class="caret"></span></a>
+						<ul class="dropdown-menu" role="menu">
+							<li><a href="#">Tele</a></li>
+							<li><a href="#">Specchiere</a></li>
+							<li><a href="#">Orologi</a></li>
+							<!--<li class="divider"></li>
+							<li class="dropdown-header">Nav header</li>
+							<li><a href="#">Separated link</a></li>
+							<li><a href="#">One more separated link</a></li>-->
+						</ul>
+					</li>
+					<li><a href="#">Contatti</a></li>
+
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+		<!--/.container-fluid -->
+	</nav>
+	<!-- end navbar -->
+
+	<!-- content -->
+	<div class="content container">
+		<row>
+			<div id="info" class="col-xs-12 col-md-4">
+				<div class="col-xs-12">
+					<h2>Contatti</h2>
+					<p>Titolare: Sporzon Tiziano</p>
+					<p>Email: 
+						<a href="mailto:emailprova@prova.it">emailprova@prova.it</a>
+					</p>
+					<p>Indirizzo: Via Brunacci, 5 - 30175 Marghera (VE)</p>
+					<p>Telefono: (+39) 041 936899</p>
+					<p>Fax: (+39) 041 5380877</p>
+					<p>CF e P.Iva: 03078210279</p>
+				</div>
+				<div class="col-xs-12">
+					<a href="#">
+						<button class="btn btn-secondary btn-lg">Scrivici una mail!</button>
+					</a>
+				</div>
+			</div>
+		</row>
+		<row>
+			<div class="col-xs-12 col-md-8">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<div class="embed-responsive embed-responsive-16by9">
+							<iframe "map embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2798.348016428484!2d12.214251315012639!3d45.462792941559314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x477eb68e7041b13f%3A0x54d0c886bf3e7ef4!2sNew+Art+Production+Srl!5e0!3m2!1sit!2sit!4v1518607827902" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</row>
+	</div>
+	<!-- end content -->
+	
+
+	<!-- footer -->
+	<footer class="footer">
+		<div class="container text-center">
+			<span class="text-muted">New Art Production Srl Via Brunacci, 5 - 30175 Marghera (VE) - Tel (+39) 041 936899 - Fax (+39) 041 5380877 - CF e P.Iva 03078210279</span>
+		</div>
+	</footer>
+	<!-- end footer -->
+
+
+</body>
+</html>
